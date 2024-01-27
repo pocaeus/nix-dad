45c31e1d8a05f91121c55cfc34c402ba0cb1712a
Pseudomata <pseudomata@proton.me>
2024-01-27 09:41:33 -0500

    Much pride, such wow

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"math/rand"
+	"text/template"
 )
 
 func main() {
-	dad := `⢸⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⡷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	arr := []string{
+		"I'm proud of you, son!",
+		"You make me so proud!",
+		"Every day makes me proud of you!",
+		"Such wow, much pride!",
+		"Much pride, such wow!",
+	}
+
+	index := rand.Intn(len(arr))
+    selected := arr[index]
+
+	dadTemplate := `
+⢸⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⡷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠢⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠈⠑⢦⡀⠀⠀⠀⠀⠀I'm proud of you, son.
+⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠈⠑⢦⡀⠀⠀{{.Message}}
 ⢸⠀⠀⠀⠀⢀⠖⠒⠒⠒⢤⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀
 ⢸⠀⠀⣀⢤⣼⣀⡠⠤⠤⠼⠤⡄⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠙⢄⠀⠀⠀⠀
 ⢸⠀⠀⠑⡤⠤⡒⠒⠒⡊⠙⡏⠀⢀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠢⡄⠀
@@ -21,6 +35,21 @@ func main() {
 ⢸⠀⠀⠀⠀⠀⢸⠀⢇⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸
 ⢸⠀⠀⠀⠀⠀⡌⠀⠈⡆⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸
 ⢸⠀⠀⠀⠀⢠⠃⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸
-⢸⠀⠀⠀⠀⢸⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠷`
-	fmt.Println(dad)
+⢸⠀⠀⠀⠀⢸⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠷
+`
+
+	tmpl, err := template.New("dad").Parse(dadTemplate)
+	if err != nil {
+	    panic(err)
+	}
+
+	data := struct {
+	    Message string
+	}{
+	    Message: selected,
+	}
+	err = tmpl.Execute(os.Stdout, data)
+	if err != nil {
+        panic(err)
+    }
 }
